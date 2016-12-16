c91c72086f737e47c4f59f0848bf8b5f97e38f11
Jeffrey Bauer <Jeffrey@Jeffreys-Air.attlocal.net>
2016-12-16 13:55:35 -0500

    updates to customer select

--- bamazoncustomer.js
+++ bamazoncustomer.js
@@ -24,11 +24,33 @@ connection.query("SELECT * FROM products", function (err, res) {
 console.log("-----------------------------------");
 }); 
 
-// var idSearch = function() {
-//   inquirer.prompt({
-//     name: "action",
-//     type: "list",
-//     message: "What product ID would you like to buy?",
-//     choices: ["Find songs by artist", "Find all artists who appear more than once",
-//       "Find data within a specific range", "Search for a specific song"]
-//   }).then(function(answer) {
+connection.end();
+
+var idSearch = function() {
+    connection.query("SELECT * FROM products", function(err, res) {
+    console.log(res);
+  inquirer.prompt({
+    name: "choice",
+    type: "rawlist",
+    choices: function(value) {
+        var choiceArray = [];
+        for (var i = 0; i < res.length; i++) {
+          choiceArray.push(res[i].item_id);
+        } 
+        return choiceArray;
+    },
+    message: "What item ID would you like to purchase?"
+  }).then(function(answer){
+    var query = "SELECT * FROM products WHERE?";
+      for (var i = 0; i < res.length; i++) {
+        if (res[i].item_id === answer.choices) {
+            var chosenItem = res[i];
+          inquirer.prompt({
+            name: "quantity",
+            type: "input",
+            message: "How many would you like to buy?"
+    }).then(function(answer){
+        for (var i = 0; i < res.length; i++) {
+            if(answer === res[i].stock_quantity){
+                
+            }
